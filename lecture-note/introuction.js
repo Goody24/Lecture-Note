daed10c820c1d3fd23e93dc1099374c5afe9e68e
OnyensoGoodluck <onyensogoodluck28@gmail.com>
2025-10-22 15:01:49 +0100

    fix(Lecture Note): Place all the files into a folder name "lecture note"

---
+++ lecture-note/introuction.js
@@ -0,0 +1,15 @@
+let day = "Tuesday";
+
+switch (day) {
+  case "Monday":
+    console.log("Start of the week.");
+    break;
+  case "Tuesday":
+    console.log("Second day of the week.");
+    break;
+  case "Friday":
+    console.log("Weekend is near!");
+    break;
+  default:
+    console.log("Just another day.");
+}
